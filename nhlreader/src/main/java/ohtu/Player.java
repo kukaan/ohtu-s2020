aaf51126bbe6defde17b718dd8a5ee37e3f16e7b
kukaan <58886775+kukaan@users.noreply.github.com>
2020-11-13 19:14:17 +0200

    add methods to Player

--- nhlreader/src/main/java/ohtu/Player.java
+++ nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,14 @@
 
 package ohtu;
 
-public class Player {
+public class Player implements Comparable<Player>{
     private String name;
+    private String nationality;
+    private String team;
+    private int assists;
+    private int goals;
+    private int penalties;
+    private int games;
 
     public void setName(String name) {
         this.name = name;
@@ -14,7 +20,59 @@ public class Player {
 
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team  + " goals " + goals + " assists " + assists;
     }
-      
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    @Override
+    public int compareTo(Player player) {
+        return player.getGoals() + player.getAssists() - this.getGoals() - this.getAssists();
+    }
 }
